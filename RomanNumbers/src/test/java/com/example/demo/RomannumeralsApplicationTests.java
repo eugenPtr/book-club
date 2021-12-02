718435eb7805b0047188213db6e6c4403c05d55f
malinaflorea <floreamalina@gmail.com>
2021-12-02 18:50:29 +0000

    added tests for 7 to 11 and refactor

--- RomanNumbers/src/test/java/com/example/demo/RomannumeralsApplicationTests.java
+++ RomanNumbers/src/test/java/com/example/demo/RomannumeralsApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -17,71 +17,82 @@ class RomannumeralsApplicationTests {
 
 	@Test
 	void test_NumberOneIsTranslatedCorrectly () {
-
 		assertEquals(arabicToRoman(1), "I");
-
 	}
 
 	@Test
 	void test_NumberTwoIsTranslatedCorrectly () {
-
 		assertEquals(arabicToRoman(2), "II");
-
 	}
 
 	@Test
 	void test_NumberThreeIsTranslatedCorrectly () {
-
 		assertEquals(arabicToRoman(3), "III");
-
 	}
 
 	@Test
 	void test_NumberFourIsTranslatedCorrectly () {
-
 		assertEquals(arabicToRoman(4), "IV");
-
 	}
 
     @Test
     void test_NumberFiveIsTranslatedCorrectly () {
-
         assertEquals(arabicToRoman(5), "V");
-
     }
 
     @Test
     void test_NumberSixIsTranslatedCorrectly () {
-
         assertEquals(arabicToRoman(6), "VI");
-
     }
 
-//	@Test
-//	void test_NumberFourteenIsTranslatedCorrectly () {
-//
-//		assertEquals(arabicToRoman(14), "XIV");
-//
-//	}
+    @Test
+    void test_NumberSevenIsTranslatedCorrectly () {
+        assertEquals(arabicToRoman(7), "VII");
+    }
 
+    @Test
+    void test_NumberEightIsTranslatedCorrectly () {
+        assertEquals(arabicToRoman(8), "VIII");
+    }
+
+    @Test
+    void test_NumberNineIsTranslatedCorrectly () {
+        assertEquals(arabicToRoman(9), "IX");
+    }
+
+    @Test
+    void test_NumberTenIsTranslatedCorrectly () {
+        assertEquals(arabicToRoman(10), "X");
+    }
+
+    @Test
+    void test_NumberElevenIsTranslatedCorrectly () {
+        assertEquals(arabicToRoman(11), "XI");
+    }
 
 
 	private String arabicToRoman(int i) {
         StringBuilder result = new StringBuilder();
-        Map<Integer, String> arabicToRomanMap = new HashMap(){{
+        Map<Integer, String> arabicToRomanMap = new LinkedHashMap<>(){{
 	        put(1, "I");
 	        put(5, "V");
+            put(10, "X");
         }};
 
-        if (i == 4) return "IV";
+        if (i == 10-1) return arabicToRomanMap.get(1) + arabicToRomanMap.get(10);
+        if (i == 5-1) return arabicToRomanMap.get(1) + arabicToRomanMap.get(5);
 
-        if (i >= 5) {
-            result.append(arabicToRomanMap.get(5));
-            i -= 5;
+        while (i > 0) {
+            int biggestValue = 0;
+            for (Map.Entry<Integer, String> pair: arabicToRomanMap.entrySet()) {
+                if (pair.getKey() > biggestValue && pair.getKey() <= i) {
+                    biggestValue = pair.getKey();
+                }
+            }
+
+            result.append(arabicToRomanMap.get(biggestValue));
+            i -= biggestValue;
         }
-
-		for (int nr = 1; nr <= i; nr++)
-			result.append(arabicToRomanMap.get(1));
 
 		return result.toString();
 	}
