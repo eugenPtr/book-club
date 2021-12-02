5e2924166691da9592eb9c5800ccbc798955fb73
malinaflorea <floreamalina@gmail.com>
2021-12-02 18:12:45 +0000

    added hash map cases five and six

--- RomanNumbers/src/test/java/com/example/demo/RomannumeralsApplicationTests.java
+++ RomanNumbers/src/test/java/com/example/demo/RomannumeralsApplicationTests.java
@@ -3,6 +3,9 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -40,6 +43,20 @@ class RomannumeralsApplicationTests {
 
 	}
 
+    @Test
+    void test_NumberFiveIsTranslatedCorrectly () {
+
+        assertEquals(arabicToRoman(5), "V");
+
+    }
+
+    @Test
+    void test_NumberSixIsTranslatedCorrectly () {
+
+        assertEquals(arabicToRoman(6), "VI");
+
+    }
+
 //	@Test
 //	void test_NumberFourteenIsTranslatedCorrectly () {
 //
@@ -50,11 +67,21 @@ class RomannumeralsApplicationTests {
 
 
 	private String arabicToRoman(int i) {
-		if (i == 4) return "IV";
+        StringBuilder result = new StringBuilder();
+        Map<Integer, String> arabicToRomanMap = new HashMap(){{
+	        put(1, "I");
+	        put(5, "V");
+        }};
 
-		StringBuilder result = new StringBuilder();
+        if (i == 4) return "IV";
+
+        if (i >= 5) {
+            result.append(arabicToRomanMap.get(5));
+            i -= 5;
+        }
+
 		for (int nr = 1; nr <= i; nr++)
-			result.append("I");
+			result.append(arabicToRomanMap.get(1));
 
 		return result.toString();
 	}
