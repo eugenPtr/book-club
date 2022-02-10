639cb1bd18957d4aaf371cbcf1678c47d7c7aafe
Codrin Sirboiu <codrin.sirboiu@gmail.com>
2022-02-10 20:38:29 +0200

    Naming refactorings

--- RomanNumbers/src/main/java/com/example/demo/RomanGenerator.java
+++ RomanNumbers/src/main/java/com/example/demo/RomanGenerator.java
@@ -2,29 +2,29 @@ package com.example.demo;
 
 import java.util.Map;
 
-public class RomanParser {
+public class RomanGenerator {
     public static final int MILS = 1000;
     private final Initializer initializer = new Initializer();
 
-    public String arabicToRoman(int i) {
+    public String from(int arabicNumber) {
         StringBuilder result = new StringBuilder();
         Map<Integer, String> arabicToRomanMap = initializer.getArabicToRomanMap();
 
-        if (i > 3999) {
-            result.append("(").append(arabicToRoman(i / MILS)).append(")");
-            i -= i / MILS * MILS;
+        if (arabicNumber > 3999) {
+            result.append("(").append(from(arabicNumber / MILS)).append(")");
+            arabicNumber -= arabicNumber / MILS * MILS;
         }
 
-        while (i > 0) {
+        while (arabicNumber > 0) {
             int biggestValue = 0;
             for (Map.Entry<Integer, String> pair : arabicToRomanMap.entrySet()) {
-                if (pair.getKey() > biggestValue && pair.getKey() <= i) {
+                if (pair.getKey() > biggestValue && pair.getKey() <= arabicNumber) {
                     biggestValue = pair.getKey();
                 }
             }
 
             result.append(arabicToRomanMap.get(biggestValue));
-            i -= biggestValue;
+            arabicNumber -= biggestValue;
         }
 
         return result.toString();
--- RomanNumbers/src/test/java/com/example/demo/RomanNumeralsApplicationTests.java
+++ RomanNumbers/src/test/java/com/example/demo/RomanNumeralsApplicationTests.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class RomanNumeralsApplicationTests {
 
-    private RomanParser romanParser = new RomanParser();
+    private final RomanGenerator romanGenerator = new RomanGenerator();
 
     @Test
 	void contextLoads() {
@@ -16,127 +16,127 @@ class RomanNumeralsApplicationTests {
 
 	@Test
 	void test_NumberOneIsTranslatedCorrectly () {
-		assertEquals(romanParser.arabicToRoman(1), "I");
+		assertEquals(romanGenerator.from(1), "I");
 	}
 
 	@Test
 	void test_NumberTwoIsTranslatedCorrectly () {
-		assertEquals(romanParser.arabicToRoman(2), "II");
+		assertEquals(romanGenerator.from(2), "II");
 	}
 
 	@Test
 	void test_NumberThreeIsTranslatedCorrectly () {
-		assertEquals(romanParser.arabicToRoman(3), "III");
+		assertEquals(romanGenerator.from(3), "III");
 	}
 
 	@Test
 	void test_NumberFourIsTranslatedCorrectly () {
-		assertEquals(romanParser.arabicToRoman(4), "IV");
+		assertEquals(romanGenerator.from(4), "IV");
 	}
 
     @Test
     void test_NumberFiveIsTranslatedCorrectly () {
-        assertEquals(romanParser.arabicToRoman(5), "V");
+        assertEquals(romanGenerator.from(5), "V");
     }
 
     @Test
     void test_NumberSixIsTranslatedCorrectly () {
-        assertEquals(romanParser.arabicToRoman(6), "VI");
+        assertEquals(romanGenerator.from(6), "VI");
     }
 
     @Test
     void test_NumberSevenIsTranslatedCorrectly () {
-        assertEquals(romanParser.arabicToRoman(7), "VII");
+        assertEquals(romanGenerator.from(7), "VII");
     }
 
     @Test
     void test_NumberEightIsTranslatedCorrectly () {
-        assertEquals(romanParser.arabicToRoman(8), "VIII");
+        assertEquals(romanGenerator.from(8), "VIII");
     }
 
     @Test
     void test_NumberNineIsTranslatedCorrectly () {
-        assertEquals(romanParser.arabicToRoman(9), "IX");
+        assertEquals(romanGenerator.from(9), "IX");
     }
 
     @Test
     void test_NumberTenIsTranslatedCorrectly () {
-        assertEquals(romanParser.arabicToRoman(10), "X");
+        assertEquals(romanGenerator.from(10), "X");
     }
 
     @Test
     void test_NumberElevenIsTranslatedCorrectly () {
-        assertEquals(romanParser.arabicToRoman(11), "XI");
+        assertEquals(romanGenerator.from(11), "XI");
     }
 
     @Test
     void test_NumberTwelveIsTranslatedCorrectly () {
-        assertEquals(romanParser.arabicToRoman(12), "XII");
+        assertEquals(romanGenerator.from(12), "XII");
     }
 
     @Test
     void test_NumberThirteenIsTranslatedCorrectly () {
-        assertEquals(romanParser.arabicToRoman(13), "XIII");
+        assertEquals(romanGenerator.from(13), "XIII");
     }
 
     @Test
     void test_NumberFourteenIsTranslatedCorrectly () {
-        assertEquals(romanParser.arabicToRoman(14), "XIV");
+        assertEquals(romanGenerator.from(14), "XIV");
     }
 
     @Test
     void test_NumberNineteenIsTranslatedCorrectly () {
-        assertEquals(romanParser.arabicToRoman(19), "XIX");
+        assertEquals(romanGenerator.from(19), "XIX");
     }
 
     @Test
     void test_NumberFortyNineIsTranslatedCorrectly () {
-        assertEquals(romanParser.arabicToRoman(49), "XLIX");
+        assertEquals(romanGenerator.from(49), "XLIX");
     }
 
     @Test
     void test_NumberFiftyIsTranslatedCorrectly () {
-        assertEquals(romanParser.arabicToRoman(50), "L");
+        assertEquals(romanGenerator.from(50), "L");
     }
 
     @Test
     void test_Number3589IsTranslatedCorrectly () {
-        assertEquals(romanParser.arabicToRoman(3589), "MMMDLXXXIX");
+        assertEquals(romanGenerator.from(3589), "MMMDLXXXIX");
     }
 
     @Test
     void test_Number3999IsTranslatedCorrectly () {
-        assertEquals(romanParser.arabicToRoman(3999), "MMMCMXCIX");
+        assertEquals(romanGenerator.from(3999), "MMMCMXCIX");
     }
 
     @Test
     void test_Number4000IsTranslatedCorrectly () {
-        assertEquals(romanParser.arabicToRoman(4000), "(IV)");
+        assertEquals(romanGenerator.from(4000), "(IV)");
     }
 
     @Test
     void test_Number4001IsTranslatedCorrectly () {
-        assertEquals(romanParser.arabicToRoman(4001), "(IV)I");
+        assertEquals(romanGenerator.from(4001), "(IV)I");
     }
 
     @Test
     void test_Number5000IsTranslatedCorrectly () {
-        assertEquals(romanParser.arabicToRoman(5000), "(V)");
+        assertEquals(romanGenerator.from(5000), "(V)");
     }
 
     @Test
     void test_Number5001IsTranslatedCorrectly () {
-        assertEquals(romanParser.arabicToRoman(5001), "(V)I");
+        assertEquals(romanGenerator.from(5001), "(V)I");
     }
 
     @Test
     void test_Number6000IsTranslatedCorrectly () {
-        assertEquals("(MMMCMXCIX)CMXCIX", romanParser.arabicToRoman(3999999));
+        assertEquals("(MMMCMXCIX)CMXCIX", romanGenerator.from(3999999));
 
     }
 
     @Test
     void test_Number4000000IsTranslatedCorrectly () {
-        assertEquals("((CD))", romanParser.arabicToRoman(400000000));
+        assertEquals("((CD))", romanGenerator.from(400000000));
     }
 }
